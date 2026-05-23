a831abb8135e58350fb5d5519464e6ea8ee30ee4
Alililaun56 <lilaunrapper@gmail.com>
2026-05-23 14:12:15 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function unlockEpisode(){
+
+window.open(
+"https://youtube.com/@novelhirmaan?si=NbH-PyVOxmIfl8Jk",
+"_blank"
+);
+
+alert("Follow and Subscribe to unlock upcoming episodes!");
+
+}
